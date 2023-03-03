483a44fafffdfaf2e6fbe45ddec04be2e3f1468f
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-03-03 12:02:26 +0000

    created scraper for first website

--- web-scrapers/elementgamesscraper.js
+++ web-scrapers/elementgamesscraper.js
@@ -1,12 +1,11 @@
-// importing dependencies required for scraper
 import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import * as fs from 'fs';
 
-async function getElementPaint()    {
+async function getElementPaint()   {
     try {
         // fetching data from url and store the response
-        const response = await fetch('https://elementgames.co.uk/paints-hobby-and-scenery/paints-washes-etc/citadel-games-workshop-paints/citadel-base');
+        const response = await fetch('https://elementgames.co.uk/paints-hobby-and-scenery/paints-washes-etc/citadel-games-workshop-paints');
         // converting the reponse to text format
         const body = await response.text();
 
@@ -17,10 +16,10 @@ async function getElementPaint()    {
         const items = [];
 
         // selecting the required classes for the paint information
-        $('.productgrid > .productinfo').map((i, el) => {
+        $('.productgrid > .productinfo').map((i, el)  =>  {
             const paintTitle = $(el).find('.producttitle').text();
             const paintPrice = $(el).find('.price').text();
-            const paintPicture = $(el).find('.productImage img').attr('src');
+            const paintPicture = $(el).find('.productimage img').attr('src');
 
             // adding items to the array
             items.push({
@@ -31,11 +30,12 @@ async function getElementPaint()    {
         });
 
         // creating .json file with results
-        var itemsString = JSON.stringify(items);
-        fs.writeFile("elementpaints.json". itemsString, function(err, result)   {
+        console.log(items);
+        var itemsString = JSON.stringify(items, null, 2);
+        fs.writeFile("./paint-data/elementgames-base-paints.json", itemsString, function(err, result)  {
             if(err) console.log('error', err);
         });
-        
+
     } catch (error) {
         console.log(error);
     }
