690cdd5d3f26478c4b16dbc6d37800aad829b506
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-03-17 13:36:18 +0000

    modified web scraper

--- web-scrapers/goblingaming/goblingaming-citadel-scraper.js
+++ web-scrapers/goblingaming/goblingaming-citadel-scraper.js
@@ -1,42 +1,85 @@
-import * as cheerio from 'cheerio';
-import fetch from 'node-fetch';
-import * as fs from 'fs';
+const puppeteer = require('puppeteer')
 
-async function getGoblinCitadelPaint()   {
+async function tutorial()   {
     try {
-        // fetching data from url and store the response
-        const response = await fetch('https://www.goblingaming.co.uk/collections/paints-citadel');
-        // converting the reponse to text format
-        const body = await response.text();
+        const URL = 'https://www.goblingaming.co.uk/collections/paints-citadel'const browser = await puppeteer.launch({headless: false})
 
-        // loading body data
-        const $ = cheerio.load(body);
-
-        // creating an empty array
-        const items = [];
-
-        // selecting the required classes for the paint information
-        $('.product-grid > .product-grid__item').map((i, el)  =>  {
-            const paintTitle = $(el).find('.product__title').text();
-            const paintPrice = $(el).find('.product__price').text();
-
-            // adding items to the array
-            items.push({
-                paintTitle,
-                paintPrice
-            });
-        });
-
-        // creating .json file with results
-        console.log(items);
-        var itemsString = JSON.stringify(items, null, 2);
-        fs.writeFile("../paint-data/goblin-citadel-paint.json", itemsString, function(err, result)  {
-            if(err) console.log('error', err);
-        });
-
+        await page.goto(URL)
+        let pagesToScrape = 16;
+        let currentPage = 1;
+        let data = []
+        while (currentPage <= pagesToScrape)    {
+            let newResults = await page.evaluate(() =>  {
+                let results = []
+                let items = document.querySelectorAll('.product-grid__item')
+                items.forEach((item) => {
+                    results.push({
+                        text: item.querySelector(class='product__title').innerText,
+                    })
+                })
+                return results
+            })
+            data = data.concat(newResults)
+            if (currentPage < pagesToScrape) {
+                await page.click('.next a')
+                await page.waitForSelector('.product-grid__item')
+                await page.waitForSelector('.next a')
+            }
+            currentPage++;
+        }
+        console.log(data)
+        await browser.close()
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
-getGoblinCitadelPaint();
+tutorial();
+
+
+
+
+
+
+// import * as cheerio from 'cheerio';
+// import fetch from 'node-fetch';
+// import * as fs from 'fs';
+
+// async function getGoblinCitadelPaint()   {
+//     try {
+//         // fetching data from url and store the response
+//         const response = await fetch('https://www.goblingaming.co.uk/collections/paints-citadel');
+//         // converting the reponse to text format
+//         const body = await response.text();
+
+//         // loading body data
+//         const $ = cheerio.load(body);
+
+//         // creating an empty array
+//         const items = [];
+
+//         // selecting the required classes for the paint information
+//         $('.product-grid > .product-grid__item').map((i, el)  =>  {
+//             const paintTitle = $(el).find('.product__title').text();
+//             const paintPrice = $(el).find('.product__price').text();
+
+//             // adding items to the array
+//             items.push({
+//                 paintTitle,
+//                 paintPrice
+//             });
+//         });
+
+//         // creating .json file with results
+//         console.log(items);
+//         var itemsString = JSON.stringify(items, null, 2);
+//         fs.writeFile("../paint-data/goblin-citadel-paint.json", itemsString, function(err, result)  {
+//             if(err) console.log('error', err);
+//         });
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getGoblinCitadelPaint();
