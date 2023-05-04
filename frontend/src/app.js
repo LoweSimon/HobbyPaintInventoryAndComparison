d70b0e112217d4ae16ec15e9bc55b661f654d6a6
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-05-04 11:13:14 +0100

    Adding commits

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,8 @@
 import React from "react";
 import NavMenu from './components/NavMenu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import dbConnect from './backend/db/dbconnect';
 import RegisterForm from "./components/RegisterForm";
 
-dbConnect();
 
 export default function App() {
     return (
