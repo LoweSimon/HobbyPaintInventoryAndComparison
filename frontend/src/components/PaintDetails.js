eb115e740a1bf65758de8f4c119157ea1786f2a2
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-08-22 17:42:15 +0100

    Created Citadel Page with data

--- frontend/src/components/PaintDetails.js
+++ frontend/src/components/PaintDetails.js
@@ -1,4 +1,4 @@
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Row, Col, Container } from 'react-bootstrap';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 const PaintDetails = ({ paint }) => {
@@ -6,29 +6,35 @@ const PaintDetails = ({ paint }) => {
     const { user } = useAuthContext();
 
     return (
-        <Card className='bg-info m-4 w-25'>
-            <Card.Body>
-                <Card.Img variant='top' src={paint.paintImage} alt="Image showing the paint" />
-                    <div className="paint-details">
-                        <Card.Title className='text-center pt-2'>{paint.paintTitle} - {paint.paintCompany}</Card.Title>
-                        <p><strong>Type: </strong>{paint.paintType}</p>
-                        <p><strong>Prices: </strong></p>
-                        <ul>
-                            <a href={paint.paintLink.elementLink}><li>Element Games: {paint.paintPrice.elementPrice}</li></a>
-                            <a href={paint.paintLink.waylandLink}><li>Wayland Games: {paint.paintPrice.waylandPrice}</li></a>
-                            <a href={paint.paintLink.hobbyworkshopPrice}><li>Hobby Workshop: {paint.paintPrice.hobbyworkshopPrice}</li></a>
-                        </ul>
-                    </div>
-                        {user && (
-                        <div className="buttons">
-                                
-                            <Button variant='primary'>Add to inventory</Button>
-                            <Button variant='primary'>Add to wishlist</Button>
-                                    
-                        </div>
-                            )}
-            </Card.Body>
-        </Card>    
+        <Container fluid>
+            <Row lg={3} md={2} sm={1}>
+                <Col>
+                    <Card className='bg-info mx-auto m-2'>
+                        <Card.Body>
+                            <Card.Img variant='top' src={paint.paintImage} alt="Paint Image" height="50%" width="100%" />
+                                <div className="paint-details">
+                                    <Card.Title className='text-center pt-2'>{paint.paintTitle} - {paint.paintCompany}</Card.Title>
+                                    <p><strong>Type: </strong>{paint.paintType}</p>
+                                    <p><strong>Prices: </strong></p>
+                                    <ul>
+                                        <a href={paint.paintLink.elementLink} target="_blank" rel='noreferrer'><li>Element Games: {paint.paintPrice.elementPrice}</li></a>
+                                        <a href={paint.paintLink.waylandLink} target="_blank" rel='noreferrer'><li>Wayland Games: {paint.paintPrice.waylandPrice}</li></a>
+                                        <a href={paint.paintLink.hobbyworkshopPrice} target="_blank" rel='noreferrer'><li>Hobby Workshop: {paint.paintPrice.hobbyworkshopPrice}</li></a>
+                                    </ul>
+                                </div>
+                                    {user && (
+                                    <div className="buttons">
+                                            
+                                        <Button variant='primary'>Add to inventory</Button>
+                                        <Button variant='primary'>Add to wishlist</Button>
+                                                
+                                    </div>
+                                        )}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>        
     )
 }
 
--- frontend/src/pages/citadelpage.js
+++ frontend/src/pages/citadelpage.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 
-import { usePaintsContext } from './../hooks/usePaintContext.js';
+import { usePaintsContext } from '../hooks/usePaintContext.js';
 // import { useAuthContext } from './../hooks/useAuthContext.js';
 
 import PaintDetails from "../components/PaintDetails.js";
 
 
 
-export default function Home() {
+export default function CitadelPage() {
 
     const {paints, dispatch} = usePaintsContext()
     // const {user} = useAuthContext()
@@ -38,16 +38,20 @@ export default function Home() {
     }, [dispatch])
 
     return (
-        
-                <Container className="border border-primary rounded bg-secondary">
-                    <div className="row">
-                        <div className="paints">
-                            {paints && paints.map((paint) => (
-                                <PaintDetails key={paint._id} paint={paint} />
-                            ))}
-                        </div>
-                    </div>        
+        <>
+            <h1 className="title text-center">Citadel Paint</h1>
+            
+                    <Container className="border border-primary rounded bg-secondary">
+                        <div className="row">
+                            <div className="paints">
+                                {paints && paints.map((paint) => (
+                                    <PaintDetails key={paint._id} paint={paint} />
+                                ))}
+                            </div>
+                        </div>        
 
-                </Container>
+                    </Container>
+
+        </>        
     )
 }
