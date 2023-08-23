21e43e09ddd767c6cba70b41127028423e1a2fae
LoweSimon <simon.lowe90@hotmail.co.uk>
2023-08-23 16:30:08 +0100

    Added data to frontend to show side by side

--- frontend/src/components/PaintDetails.js
+++ frontend/src/components/PaintDetails.js
@@ -1,41 +1,54 @@
-import { Button, Card, Row, Col, Container } from 'react-bootstrap';
+import { Button, Card, Row, Col, Container} from 'react-bootstrap';
 import { useAuthContext } from '../hooks/useAuthContext';
+import { usePaintsContext } from '../hooks/usePaintContext.js';
 
 const PaintDetails = ({ paint }) => {
 
     const { user } = useAuthContext();
+    const { paints } = usePaintsContext();
+
+    
+
 
     return (
-        <Container fluid>
-            <Row lg={3} md={2} sm={1}>
-                <Col>
-                    <Card className='bg-info mx-auto m-2'>
-                        <Card.Body>
-                            <Card.Img variant='top' src={paint.paintImage} alt="Paint Image" height="50%" width="100%" />
-                                <div className="paint-details">
-                                    <Card.Title className='text-center pt-2'>{paint.paintTitle} - {paint.paintCompany}</Card.Title>
-                                    <p><strong>Type: </strong>{paint.paintType}</p>
-                                    <p><strong>Prices: </strong></p>
-                                    <ul>
-                                        <a href={paint.paintLink.elementLink} target="_blank" rel='noreferrer'><li>Element Games: {paint.paintPrice.elementPrice}</li></a>
-                                        <a href={paint.paintLink.waylandLink} target="_blank" rel='noreferrer'><li>Wayland Games: {paint.paintPrice.waylandPrice}</li></a>
-                                        <a href={paint.paintLink.hobbyworkshopPrice} target="_blank" rel='noreferrer'><li>Hobby Workshop: {paint.paintPrice.hobbyworkshopPrice}</li></a>
-                                    </ul>
-                                </div>
-                                    {user && (
-                                    <div className="buttons">
-                                            
-                                        <Button variant='primary'>Add to inventory</Button>
-                                        <Button variant='primary'>Add to wishlist</Button>
-                                                
+
+        <Row lg={4}>
+            {paints &&
+                paints.map((product) => {
+                const { id, paintTitle, paintType, paintImage, paintCompany, elementLink, waylandLink, hobbyworkshopLink, elementPrice, waylandPrice, hobbyworkshopPrice } = product;
+                return (
+                    <Container>
+                    <Col className="d-flex">
+                        <Card className="flex-fill bg-info mx-auto m-2 flex-fill" key={id}>
+                            <Card.Body>
+                                <Card.Img variant='top' src={paintImage} alt="Paint Image" height="50%" width="100%" />
+                                    <div className="paint-details">
+                                        <Card.Title className='text-center pt-2'>{paintTitle} - {paintCompany}</Card.Title>
+                                        <p><strong>Type: </strong>{paintType}</p>
+                                        <p><strong>Prices: </strong></p>
+                                        <ul>
+                                            <a href={elementLink} target="_blank" rel='noreferrer'><li>Element Games: {elementPrice}</li></a>
+                                            <a href={waylandLink} target="_blank" rel='noreferrer'><li>Wayland Games: {waylandPrice}</li></a>
+                                            <a href={hobbyworkshopLink} target="_blank" rel='noreferrer'><li>Hobby Workshop: {hobbyworkshopPrice}</li></a>
+                                        </ul>
                                     </div>
-                                        )}
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>        
+                                        {user && (
+                                        <div className="buttons">
+                                                    
+                                            <Button variant='primary'>Add to inventory</Button>
+                                            <Button variant='primary'>Add to wishlist</Button>
+                                                        
+                                            </div>
+                                                )}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    </Container>
+                );
+                })}
+        </Row> 
     )
 }
+
 
 export default PaintDetails;
--- frontend/src/pages/citadelpage.js
+++ frontend/src/pages/citadelpage.js
@@ -1,28 +1,20 @@
 import React from "react";
 import { useEffect } from "react";
-import { Container } from "react-bootstrap";
 
 import { usePaintsContext } from '../hooks/usePaintContext.js';
-// import { useAuthContext } from './../hooks/useAuthContext.js';
 
 import PaintDetails from "../components/PaintDetails.js";
+import { Container } from 'react-bootstrap';
 
 
 
 export default function CitadelPage() {
 
-    const {paints, dispatch} = usePaintsContext()
-    // const {user} = useAuthContext()
+    const { dispatch} = usePaintsContext()
 
     useEffect(() => {
         const fetchPaints = async () => {
-            const response = await fetch('http://localhost:4000/api/citadel' 
-            // {
-            //     headers: {
-            //         'Authorisation': `Bearer ${user.token}`
-            //     },
-            // }
-            )
+            const response = await fetch('http://localhost:4000/api/citadel')
             const json = await response.json()
 
             if (response.ok) {
@@ -30,27 +22,18 @@ export default function CitadelPage() {
             }
         }
 
-        // if (user) {
-        //     fetchPaints()
-        // }
         fetchPaints()
 
     }, [dispatch])
 
     return (
         <>
-            <h1 className="title text-center">Citadel Paint</h1>
-            
-                    <Container className="border border-primary rounded bg-secondary">
-                        <div className="row">
-                            <div className="paints">
-                                {paints && paints.map((paint) => (
-                                    <PaintDetails key={paint._id} paint={paint} />
-                                ))}
-                            </div>
-                        </div>        
 
-                    </Container>
+        <h1 className="title text-center">Citadel Paint</h1>
+
+        <Container className="border border-primary rounded bg-secondary">
+            <PaintDetails />
+        </Container>
 
         </>        
     )
